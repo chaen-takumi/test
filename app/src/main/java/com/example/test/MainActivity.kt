8592382dc1ec8812c029f843045fde12d0c17b45
chaen-takumi <chaen.ta1227@gmail.com>
2023-02-05 21:39:29 +0900

    Test
    Test

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //１）idを取得
+        val tx: TextView = findViewById(R.id.tv)
+        val btn1: Button = findViewById(R.id.btnRice)
+        val btn2: Button = findViewById(R.id.btnBread)
+        val btn3: Button = findViewById(R.id.btnClear)
+
+        //２）クリック処理
+        btn1.setOnClickListener {
+            tx.text = "米"
+        }
+        btn2.setOnClickListener {
+            tx.text = "パン"
+        }
+        btn3.setOnClickListener {
+            tx.text = "..."
+        }
+
+    }
+}
